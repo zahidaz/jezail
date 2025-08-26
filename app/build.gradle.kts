759c7f259568eb69f371e9f4ea45cb84addcfe27
zahidaz <me.az.zahid@gmail.com>
2025-08-26 00:16:05 -0700

    feat: added extended icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
+
 
     implementation(libs.libsu.core)
     implementation(libs.service)
