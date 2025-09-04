c3b72ab74f89d89b974a043b03b550d09ab383b0
zahidaz <me.az.zahid@gmail.com>
2025-09-04 00:10:05 -0700

    fix: sem version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.azzahid.jezail"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.$versionCode"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
